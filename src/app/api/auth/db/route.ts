e39c44920d7b20620b1e9220fd40b07aea5f8297
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-11-04 11:18:11 -0500

    feat: db

---
+++ src/app/api/auth/db/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { pb } from '../../services/db'
+
+export async function GET (res: NextRequest, req: NextRequest) {
+//   const record = await pb.collection('users').getOne('9whcad722pwje9t',{
+//     fields: 'name'
+//   })
+//   return NextResponse.json({ record })
+}
