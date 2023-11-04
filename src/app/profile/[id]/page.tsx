46ac719b7356735440895ab5051d107c2f8b1cb7
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-11-04 13:26:05 -0500

    feat: add profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { Button, Divider, User } from '@nextui-org/react';
 
 type User = {
   avatar: string;
@@ -21,21 +22,62 @@ type User = {
 
 function Page({ params }: { params: { id: string } }) {
   const [data, setData] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(params);
   useEffect(() => {
     async function getData() {
-      const response: User = await axios.get('/api/get-user', {
-        headers: {
-          userId: params.id,
-        },
-      });
-      setData(response);
+      try {
+        const response: AxiosResponse<User> = await axios.get('/api/get-user', {
+          headers: {
+            userId: params.id,
+          },
+        });
+        console.log(response);
+        setIsLoading(false);
+        setData(response.data);
+      } catch (e) {
+        console.error(e);
+      }
     }
+
+    if (params.id && isLoading) getData();
   }, []);
-  console.debug(data)
+  console.debug(data);
+
+  if (isLoading) {
+    return (
+      <main>
+        <h4>Loading...</h4>
+      </main>
+    );
+  }
 
   return (
     <main>
-      <h4></h4>
+      <div className='py-10 px-2 flex flex-col gap-2'>
+        <User
+          name={data?.username}
+          description='Fitness influencer'
+          avatarProps={{
+            src: 'https://i.pravatar.cc/150?img=68',
+          }}
+        />
+        <div className='flex flex-col gap-2 w-full '>
+          <div className='flex flex-row gap-4 justify-center w-full'>
+            <div className='flex flex-col justify-center items-center'>
+              <h3>Followers</h3>
+              {data?.follower}
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <h3>Following</h3>
+              {data?.following}
+            </div>
+          </div>
+          <Button>Follow</Button>
+        </div>
+      </div>
+      <Divider />
+      <div></div>
     </main>
   );
 }
